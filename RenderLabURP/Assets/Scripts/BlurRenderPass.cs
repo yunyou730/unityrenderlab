581bc9acc5e31a4b036c53738cfcd625b4f95b44
ayy730 <502765299@qq.com>
2024-07-07 20:42:31 +0800

    +blur render pass add log

--- RenderLabURP/Assets/Scripts/BlurRenderPass.cs
+++ RenderLabURP/Assets/Scripts/BlurRenderPass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.UI;
 using UnityEngine.Rendering.Universal;
 
 public class BlurRenderPass : ScriptableRenderPass
@@ -15,7 +16,7 @@ public class BlurRenderPass : ScriptableRenderPass
     
     private static readonly int _horizontalBlurId = Shader.PropertyToID("_HorizontalBlur");
     private static readonly int _verticalBlurId = Shader.PropertyToID("_VerticalBlur");
-
+    
     public BlurRenderPass(BlurSettings settings,Material material)
     {
         _defaultSettings = settings;
@@ -48,6 +49,7 @@ public class BlurRenderPass : ScriptableRenderPass
         // Blit from the temporary render texture to the camera target,
         // using the second shader pass.
         Blit(cmd, _blurTextureHandle, cameraTargetHandle, _material, 1);
+        //Blit(cmd,_blurTextureHandle,cameraTargetHandle);
 
         //Execute the command buffer and release it back to the pool.
         context.ExecuteCommandBuffer(cmd);
@@ -84,17 +86,24 @@ public class BlurRenderPass : ScriptableRenderPass
     
     private void UpdateBlurSettings()
     {
-        if (_material == null) return;
+        if (_material == null) 
+            return;
 
         // Use the Volume settings or the default settings if no Volume is set.
-        var volumeComponent =
-            VolumeManager.instance.stack.GetComponent<CustomVolumeComponent>();
+        var volumeComponent = VolumeManager.instance.stack.GetComponent<CustomVolumeComponent>();
+
         float horizontalBlur = volumeComponent.horizontalBlur.overrideState ?
             volumeComponent.horizontalBlur.value : _defaultSettings.horizontalBlur;
         float verticalBlur = volumeComponent.verticalBlur.overrideState ?
             volumeComponent.verticalBlur.value : _defaultSettings.verticalBlur;
+        
+        
+        Debug.Log($"blur factor: horizontal:{horizontalBlur},vertical:{verticalBlur}");
         _material.SetFloat(_horizontalBlurId, horizontalBlur);
         _material.SetFloat(_verticalBlurId, verticalBlur);
+        
+        // _material.SetFloat(_horizontalBlurId, 0);
+        // _material.SetFloat(_verticalBlurId, 0);        
     }
     
     
